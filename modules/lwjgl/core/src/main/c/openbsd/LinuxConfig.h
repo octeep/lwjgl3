621ddefd42ee629fce5c3884fb3b744f37c97055
Kurt Mosiejczuk <kurt@cranky.work>
2020-03-09 15:13:13 -0400

    Clang doesn't like "-Wunused-but-set-variable"

--- modules/lwjgl/core/src/main/c/openbsd/LinuxConfig.h
+++ modules/lwjgl/core/src/main/c/openbsd/LinuxConfig.h
@@ -9,7 +9,6 @@
 #define DISABLE_WARNINGS() \
     _Pragma("GCC diagnostic push")
     _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
-    _Pragma("GCC diagnostic ignored \"-Wunused-but-set-variable\"")
     _Pragma("GCC diagnostic ignored \"-Wsign-compare\"")
 
 #define ENABLE_WARNINGS() \
